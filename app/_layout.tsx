989dacec8205fe2296bcffbc7b0bd58099945d21
lumir.ludvik <lumir.ludvik@ext.ehealthexperts.de>
2025-03-14 15:40:26 +0100

    initial project

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,91 @@
-import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
+import {NavigationContainer, NavigationIndependentTree} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import {GlobalStyles} from "@/constants/styles";
+import IconButton from "@/components/UI/IconButton";
+import RecentExpenses from "@/screens/RecentExpenses";
+import AllExpenses from "@/screens/AllExpenses";
+import ExpensesContextProvider from "@/store/expenses-context";
+import ManageExpense from "@/screens/ManageExpense";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+      <BottomTabs.Navigator
+          screenOptions={({ navigation }) => ({
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white',
+            tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            tabBarActiveTintColor: GlobalStyles.colors.accent500,
+            headerRight: ({ tintColor }) => (
+                <IconButton
+                    icon="add"
+                    size={24}
+                    color={tintColor}
+                    onPress={() => {
+                      navigation.navigate('ManageExpense');
+                    }}
+                />
+            ),
+          })}
+      >
+        <BottomTabs.Screen
+            name="RecentExpenses"
+            component={RecentExpenses}
+            options={{
+              title: 'Recent Expenses',
+              tabBarLabel: 'Recent',
+              tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="hourglass" size={size} color={color} />
+              ),
+            }}
+        />
+        <BottomTabs.Screen
+            name="AllExpenses"
+            component={AllExpenses}
+            options={{
+              title: 'All Expenses',
+              tabBarLabel: 'All Expenses',
+              tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="calendar" size={size} color={color} />
+              ),
+            }}
+        />
+      </BottomTabs.Navigator>
+  );
+}
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+<>
+  <StatusBar style="light" />
+  <ExpensesContextProvider>
+    <NavigationIndependentTree>
+      <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+            headerTintColor: 'white',
+          }}
+      >
+        <Stack.Screen
+            name="ExpensesOverview"
+            component={ExpensesOverview}
+            options={{ headerShown: false }}
+        />
+        <Stack.Screen
+            name="ManageExpense"
+            component={ManageExpense}
+            options={{
+              presentation: 'modal',
+            }}
+        />
+      </Stack.Navigator>
+    </NavigationIndependentTree>
+  </ExpensesContextProvider>
+</>
+  )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,24 @@
-import { Text, View } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import {GlobalStyles} from "@/constants/styles";
+import IconButton from "@/components/UI/IconButton";
+import RecentExpenses from "@/screens/RecentExpenses";
+import AllExpenses from "@/screens/AllExpenses";
+import ExpensesContextProvider from "@/store/expenses-context";
+import ManageExpense from "@/screens/ManageExpense";
+
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    return (
+        <>
+
+        </>
+    );
 }
